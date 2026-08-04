6984a79741f03f9807751185af4483c598dd1050
DESKTOP-MEKI\yurir <yuriruf@hotmail.com>
2026-08-04 18:41:37 +0900

    update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jaskanwal96.github.io',
+  site: 'https://yurifarion.github.io',
   base: '/game-website',
 });
