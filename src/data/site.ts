2aa2e7cf3e2e7ceb79baced95c3bd1d85ad36dcd
DESKTOP-MEKI\yurir <yuriruf@hotmail.com>
2026-08-09 08:07:12 +0900

    change domain

--- src/data/site.ts
+++ src/data/site.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
   studioName: "Yuri Farion",
   tagline: "We make games we'd want to play ourselves.",
   contactEmail: "farion.yuri@gmail.com",
-  domain: "https://placeholder-domain.com",
+  domain: "https://yurifarion.com",
   kitFormId: "9767185",
   social: {
     twitter: "https://x.com/yuriddeveloper",
